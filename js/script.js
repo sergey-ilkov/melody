3348e6c83dbec8a7476d6138825a4592dd4b4ff7
sergey-ilkov <72069299+sergey-ilkov@users.noreply.github.com>
2021-08-19 16:26:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,56 +1,53 @@
-const counter = document.querySelector('.counter');
-const homeImage = document.querySelector('.home-image');
-let defoultFloor = homeImage.querySelector(`path[data="${counter.innerHTML}"]`);
-showFloor();
-
-homeImage.addEventListener('mouseover', (event) => {
-    let floorNumber = event.target.getAttribute('data');
-    let list = homeImage.querySelectorAll('path');
-    list.forEach((element) => {
-        element.style.opacity = '0';
+$(document).ready(function () {
+    var currentFloor = 2;
+    var floorPath = $('.home-image path');
+    var counterUp = $('.counter-up');
+    var counterDown = $('.counter-down');
+    var modal = $('.modal');
+    floorPath.on('mouseover', function () {
+        floorPath.removeClass('current-floor');
+        currentFloor = $(this).attr('data');
+        $('.counter').text(currentFloor);
     });
-    event.target.style.opacity = '1';
-    if (floorNumber !== null) {
-        counter.innerHTML = floorNumber;
-    }
-    showFloor();
-});
-const counterUp = document.querySelector('.counter-up');
-counterUp.addEventListener('click', () => {
-    let floor = counter.innerHTML;
-    let floorNumber = Number(counter.innerHTML);
-    if (floorNumber < 18) {
-        floorNumber = floorNumber + 1;
-        floorNumber > 9
-            ? floorNumber
-            : (floorNumber = '0' + String(floorNumber));
-        counter.innerHTML = floorNumber;
+    counterUp.on('click', function () {
+        if (currentFloor < 18) {
+            currentFloor++;
+            usCurrentFloor = currentFloor.toLocaleString('en-US', {
+                minimumIntegerDigits: 2,
+                useGroupping: false,
+            });
+            $('.counter').text(usCurrentFloor);
+            floorPath.removeClass('current-floor');
+            $(`[data=${usCurrentFloor}]`).toggleClass('current-floor');
+        }
+    });
+    counterDown.on('click', function () {
+        if (currentFloor > 2) {
+            currentFloor--;
+            usCurrentFloor = currentFloor.toLocaleString('en-US', {
+                minimumIntegerDigits: 2,
+                useGroupping: false,
+            });
+            $('.counter').text(usCurrentFloor);
+            floorPath.removeClass('current-floor');
+            $(`[data=${usCurrentFloor}]`).toggleClass('current-floor');
+        }
+    });
 
-        chooseFloor(floor, floorNumber);
-    }
+    modal.on('click', function (e) {
+        if (
+            e.target.className == 'modal__apartment-img' ||
+            e.target.className == 'modal modal-active'
+        ) {
+            modal.removeClass('modal-active');
+        }
+    });
+    $('.button-primery').on('click', function () {
+        modal.addClass('modal-active');
+        $('.modal__floor-title').text('Этаж ' + currentFloor);
+    });
+    floorPath.on('click', function () {
+        modal.addClass('modal-active');
+        $('.modal__floor-title').text('Этаж ' + currentFloor);
+    });
 });
-const counterDown = document.querySelector('.counter-down');
-counterDown.addEventListener('click', () => {
-    let floor = counter.innerHTML;
-    let floorNumber = Number(counter.innerHTML);
-    if (floorNumber > 2) {
-        floorNumber = floorNumber - 1;
-        floorNumber > 9
-            ? floorNumber
-            : (floorNumber = '0' + String(floorNumber));
-        counter.innerHTML = floorNumber;
-
-        chooseFloor(floor, floorNumber);
-    }
-});
-function chooseFloor(floor, floorNumber) {
-    let hideFloor = homeImage.querySelector(`path[data="${floor}"]`);
-    hideFloor.style.opacity = '0';
-    let showFloor = homeImage.querySelector(`path[data="${floorNumber}"]`);
-    showFloor.style.opacity = '1';
-}
-
-function showFloor() {
-    defoultFloor = homeImage.querySelector(`path[data="${counter.innerHTML}"]`);
-    defoultFloor.style.opacity = '1';
-}
