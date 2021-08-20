19f7ee67f4fe57d90b56387c0577e05ec1c3a831
sergey-ilkov <72069299+sergey-ilkov@users.noreply.github.com>
2021-08-20 13:31:21 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,11 +4,33 @@ $(document).ready(function () {
     var counterUp = $('.counter-up');
     var counterDown = $('.counter-down');
     var modal = $('.modal');
+    var modalCloseButton = $('.modal-close-button');
+    var buttonPrimery = $('.button-primery');
+    var flats = $('.flats path');
+    var flatList = $('.flat-list .flat-link');
+    var modalCounter = $('.modal-counter');
+
     floorPath.on('mouseover', function () {
         floorPath.removeClass('current-floor');
         currentFloor = $(this).attr('data');
         $('.counter').text(currentFloor);
     });
+    floorPath.on('click', toggleModal);
+    modalCloseButton.on('click', toggleModal);
+    buttonPrimery.on('click', toggleModal);
+
+    flats.on('mouseover', function () {
+        currentFlats = $(this).attr('data-flat');
+        flatList.removeClass('flat-link-hover');
+        $(`[data-flat-link=${currentFlats}]`).toggleClass('flat-link-hover');
+    });
+    flatList.on('mouseover', function () {
+        currentFlatList = $(this).attr('data-flat-link');
+        flatList.removeClass('flat-link-hover');
+        flats.removeClass('current-flat');
+        $(`[data-flat=${currentFlatList}]`).toggleClass('current-flat');
+    });
+
     counterUp.on('click', function () {
         if (currentFloor < 18) {
             currentFloor++;
@@ -33,21 +55,21 @@ $(document).ready(function () {
             $(`[data=${usCurrentFloor}]`).toggleClass('current-floor');
         }
     });
-
-    modal.on('click', function (e) {
-        if (
-            e.target.className == 'modal__apartment-img' ||
-            e.target.className == 'modal modal-active'
-        ) {
-            modal.removeClass('modal-active');
-        }
-    });
-    $('.button-primery').on('click', function () {
-        modal.addClass('modal-active');
-        $('.modal__floor-title').text('Этаж ' + currentFloor);
-    });
-    floorPath.on('click', function () {
-        modal.addClass('modal-active');
-        $('.modal__floor-title').text('Этаж ' + currentFloor);
-    });
+    function toggleModal() {
+        modal.toggleClass('is-open');
+        currentFloor = Number(currentFloor);
+        currentFloor = String(currentFloor);
+        modalCounter.text(currentFloor);
+        flats.removeClass('current-flat');
+        flatList.removeClass('flat-link-hover');
+        $.each(flats, function (el) {
+            newCurrentFlat = currentFloor + String(el);
+            $(this).attr('data-flat', newCurrentFlat);
+        });
+        $.each(flatList, function (el) {
+            newCurrentFlat = currentFloor + String(el);
+            $(this).attr('data-flat-link', newCurrentFlat);
+            $(this).children().text(newCurrentFlat);
+        });
+    }
 });
